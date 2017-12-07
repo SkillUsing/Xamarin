5fafec9ab034ccb84468e0141849e7a2e47c95c9
李晨 <lichen@fissoft.com>
2017-12-07 18:04:34 +0800

    1

--- App1/App1/Utils/MakeTextUtils.cs
+++ App1/App1/Utils/MakeTextUtils.cs
@@ -1,6 +1,8 @@
+using Android.App;
 using Android.Bluetooth;
 using Android.Content;
 using Android.Widget;
+using System;
 
 namespace App1
 {
@@ -12,6 +14,31 @@ namespace App1
         public static void MakeText(Context context, string content)
         {
             Toast.MakeText(context, content, ToastLength.Short).Show();
+        }
+
+        public static void Alert(Activity activity, string message)
+        {
+            activity.RunOnUiThread(() =>
+            {
+                var dialog= new AlertDialog.Builder(activity);
+                dialog.SetTitle("确认").SetMessage(message).Show();
+            });
+        }
+
+        public static void MessageBox(Activity activity, string message, string msgTitle, Action yesFun, Action noFun)
+        {
+            activity.RunOnUiThread(() =>
+            {
+                var msgBox=new AlertDialog.Builder(activity);
+                msgBox.SetPositiveButton("确定", (s, e) =>
+                {
+                    var d = (AlertDialog) s;
+                    d.Cancel();
+                    yesFun();
+                });
+                msgBox.SetNegativeButton("取消", (s, e) => { noFun(); });
+                msgBox.SetTitle(msgTitle).SetMessage(message).Show();
+            });
         }
     }
 }
